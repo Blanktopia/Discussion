30dbee5f5664ddd1c55e029f605b48a02a4436ae
Goh Wei Wen <goweiwen@gmail.com>
2025-02-27 18:00:38 +0800

    Fix Discord hook format

--- src/main/kotlin/me/weiwen/discussion/hooks/DiscordSrvHook.kt
+++ src/main/kotlin/me/weiwen/discussion/hooks/DiscordSrvHook.kt
@@ -2,9 +2,6 @@ package me.weiwen.discussion.hooks
 
 import github.scarsz.discordsrv.DiscordSRV
 import me.weiwen.discussion.Discussion.Companion.plugin
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.MiniMessage
-import github.scarsz.discordsrv.dependencies.kyori.adventure.text.minimessage.MiniMessage as DiscordSRVMiniMessage
 import org.bukkit.entity.Player
 import org.bukkit.event.Event
 
@@ -17,10 +14,10 @@ object DiscordSrvHook {
                 null
             }
 
-    fun processChatMessage(player: Player, message: Component, channel: String, cancelled: Boolean, event: Event) {
+    fun processChatMessage(player: Player, message: String, channel: String, cancelled: Boolean, event: Event) {
         discordSrv?.processChatMessage(
             player,
-            DiscordSRVMiniMessage.miniMessage().deserialize(MiniMessage.miniMessage().serialize(message)),
+            message,
             channel,
             cancelled,
             event,
